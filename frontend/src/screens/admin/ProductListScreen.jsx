c59db72a34b8c148e5ae6fb4fc3e169a52bc1bb6
Aleksandar Buhinjak <40025268+abuhinjak@users.noreply.github.com>
2024-01-12 12:11:52 +0100

    Pagination

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,14 +1,18 @@
+import { useParams } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col} from 'react-bootstrap';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import { toast } from 'react-toastify';
 import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from '../../slices/productsApiSlice';
 
 
 const ProductListScreen = () => {
-    const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+
+    const { data, isLoading, error, refetch } = useGetProductsQuery({pageNumber});
 
     const [createProduct, {isLoading: loadingCreate }] = useCreateProductMutation();
 
@@ -67,7 +71,7 @@ const ProductListScreen = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {products.map((product) => (
+                            {data.products.map((product) => (
                                 <tr key={product._id}>
                                     <td>
                                         {product._id}
@@ -98,6 +102,7 @@ const ProductListScreen = () => {
                             ))}
                         </tbody>
                     </Table>
+                    <Paginate pages={data.pages} page={data.page} isAdmin={true} />
                 </>
             )}
         </>
